0b8f706a8087a8d5349ee26428a27e45bdb0529b
denizgokce <denizgokce93@gmail.com>
2018-02-11 11:13:38 +0300

    Project Finished!

--- datasource/peopleRepository.js
+++ datasource/peopleRepository.js
@@ -5,7 +5,7 @@ mongodb = require("../configuration/dbConfig")
 var peopleRepo = repo(Person, mongodb);
 
 module.exports.getPeople = function (callback) {
-    peopleRepo.find().then(function (people) {
+    peopleRepo.find({}, "-_id id firstname lastname").then(function (people) {
         return callback(null, people);
     }).catch(function (err) {
         console.log(err.message);
